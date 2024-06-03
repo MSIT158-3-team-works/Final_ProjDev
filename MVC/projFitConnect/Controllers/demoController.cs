082d0eef61717059586ced6db43fbe0f8100f8a1
HecktJims <jimmy40706@gmail.com>
2024-06-03 17:43:17 +0800

    0603/1743 demo/try1 works, others do not

---
+++ MVC/projFitConnect/Controllers/demoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projFitConnect.Controllers
+{
+    public class demoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult try1()
+        {
+            return View();
+        }
+        public IActionResult try2()
+        {
+            return View();
+        }
+    }
+}
