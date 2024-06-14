31c1320c74c51cc917439c44e4e0530987b4a69e
HecktJims <jimmy40706@gmail.com>
2024-06-14 13:42:03 +0800

    0614 for merge

---
+++ API/projRESTfulApiFitConnect/DTO/Product/AddProductDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace projRESTfulApiFitConnect.DTO.Product
+{
+    public class AddProductDTO
+    {
+        public string ProductName { get; set; } = null!;
+
+        public int? CategoryId { get; set; }
+
+        public decimal ProductUnitprice { get; set; } = 0;
+
+        public string? ProductDetail { get; set; }
+
+        public string? ProductImage { get; set; }
+    }
+}
