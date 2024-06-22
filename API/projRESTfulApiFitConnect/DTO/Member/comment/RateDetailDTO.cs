5fff2e3ae2f06230502c981307217a7942501e9b
Hank <j41415j41415@mail.com>
2024-06-22 16:58:03 +0800

    0622-1700comment

--- API/projRESTfulApiFitConnect/DTO/Member/comment/RateDetailDTO.cs
+++ API/projRESTfulApiFitConnect/DTO/Member/comment/RateDetailDTO.cs
@@ -1,12 +1,16 @@
 ﻿namespace projRESTfulApiFitConnect.DTO.Member.comment
 {
-    public class RatesDTO
+    public class RateDetailDTO
     {
-        public int RateId {  get; set; }
+        public int RateId { get; set; }
         public int ReserveId { get; set; }
         public int MemberId { get; set; }
         public int ClassId { get; set; }
+        public string? ClassName { get; set; }
+        public string? Classpic { get; set; }
         public int CoachId { get; set; }
+        public string? CoachName { get; set; }
+        public string? Coachphoto { get; set; }
         public decimal? RateClass { get; set; }
         public string? RateClassDescribe { get; set; }
         public decimal? RateCoach { get; set; }
