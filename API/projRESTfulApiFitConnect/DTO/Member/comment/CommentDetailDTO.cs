32ce1c25c86c7b4c055caa5bd0473025b4568866
承翰 周 <j41415j41415@gmail.com>
2024-06-23 02:32:02 +0800

    comment-test

--- API/projRESTfulApiFitConnect/DTO/Member/comment/CommentDetailDTO.cs
+++ API/projRESTfulApiFitConnect/DTO/Member/comment/CommentDetailDTO.cs
@@ -7,7 +7,7 @@ namespace projRESTfulApiFitConnect.DTO.Member.comment
         public int ClassReserveId { get; set; }
         public string? ClassName { get; set; }
         public int? Coach { get; set; }
-        //public string? CoachName { get; set; }
+        public string? CoachName { get; set; }
         public string? GymName { get; set; }
         public DateOnly CourseDate { get; set; }
         public TimeOnly CourseStartTime { get; set; }
