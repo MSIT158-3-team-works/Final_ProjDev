e3fefd612045b8aef019a5902b65d8a595e08707
澈 黃 <celebrate.kirby@gmail.com>
2024-06-25 20:11:56 +0800

    0625-getlogin memberID

--- MVC/projFitConnect/Controllers/SessionController.cs
+++ MVC/projFitConnect/Controllers/SessionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using projFitConnect.ViewModels;
 
 namespace projFitConnect.Controllers
 {
@@ -10,6 +11,20 @@ namespace projFitConnect.Controllers
             base.OnActionExecuted(context);
             if (!(HttpContext.Session.Keys.Contains("ID") && HttpContext.Session.Keys.Contains("role_ID")))
                 context.Result = new RedirectToRouteResult("Home Page", new { controller = "Home", action = "" });
+        }
+
+        [HttpGet]
+        public IActionResult Login() 
+        {
+            int? ID = HttpContext.Session.GetInt32("ID");
+            int? roleId = HttpContext.Session.GetInt32("role_ID");
+            if (ID == null)            
+                return NotFound();            
+            C_user user = new C_user();
+            user.id = ID.ToString();
+            user.role_id = roleId.ToString(); 
+            
+            return Ok(user);
         }
     }
 }
